6bd45fa43a1dcfd00b6791d429cb3ea5027f0877
sagie gur ari <sagiegurari@gmail.com>
2019-05-08 14:38:55 +0300

    release

---
+++ docs/api/search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=["envmnt","string","option"];
+
+searchIndex["envmnt"]={"doc":R[0],"i":[[5,"exists",R[0],"Returns true environment variable is defined.",N,[[["str"]],["bool"]]],[5,"remove",E,"Removes the provided environment variable.",N,[[["str"]]]],[5,"get_remove",E,"Removes the provided environment variable and returns its…",N,[[["str"]],[[R[2],[R[1]]],[R[1]]]]],[5,"get_or",E,"Returns the environment variable value or if is not…",N,[[["str"]],[R[1]]]],[5,"is_or",E,"Returns false if environment variable value if falsy. The…",N,[[["bool"],["str"]],["bool"]]],[5,"set",E,"Sets the environment variable value.",N,[[["str"]]]],[5,"set_bool",E,"Sets the environment variable with a true/false value as…",N,[[["bool"],["str"]]]],[5,"get_set",E,"Sets the environment variable value and returns the…",N,[[["str"]],[[R[2],[R[1]]],[R[1]]]]]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ docs/api/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["envmnt"] = {"name":"","files":["environment.rs","lib.rs","util.rs"]};
+createSourceSidebar();
