e205eb2f1ed263181b2c5d1d7fdb02cfc0d1cc66
sagie gur ari <sagiegurari@gmail.com>
2020-04-14 17:46:36 +0000

    release

---
+++ docs/api/implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["envmnt"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"envmnt/types/struct.ListOptions.html\" title=\"struct envmnt::types::ListOptions\">ListOptions</a>","synthetic":false,"types":["envmnt::types::ListOptions"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/api/implementors/std/panic/trait.RefUnwindSafe.js
+++ docs/api/implementors/std/panic/trait.RefUnwindSafe.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["envmnt"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"envmnt/types/struct.ListOptions.html\" title=\"struct envmnt::types::ListOptions\">ListOptions</a>","synthetic":true,"types":["envmnt::types::ListOptions"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"envmnt/types/struct.ExpandOptions.html\" title=\"struct envmnt::types::ExpandOptions\">ExpandOptions</a>","synthetic":true,"types":["envmnt::types::ExpandOptions"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"enum\" href=\"envmnt/types/enum.ExpansionType.html\" title=\"enum envmnt::types::ExpansionType\">ExpansionType</a>","synthetic":true,"types":["envmnt::types::ExpansionType"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
